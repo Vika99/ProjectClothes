72352da42fb3f849df863ddf80a177352eff232b
Vika99 <56167077+Vika99@users.noreply.github.com>
2020-06-12 13:12:02 +0300

    Add files via upload

--- src/Clothes/ReflectionFactory.java
+++ src/Clothes/ReflectionFactory.java
@@ -1,17 +1,15 @@
 package Clothes.MenuWithGeneric;
 
-import Clothes.Clothes;
-import Clothes.ClothesFactory;
+import Clothes.Jeans;
 import Clothes.NotSimpleMenu.ScannerWrapper;
+import Clothes.Tshirt;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 
 
 
-
-public class ReflectionFactory < E extends ClothesFactory> {
+public class ReflectionFactory  {
     private final List<Class<?>> classes;
     private ScannerWrapper sc = new ScannerWrapper();
 
@@ -23,73 +21,37 @@ public class ReflectionFactory < E extends ClothesFactory> {
         System.out.println("Choose class");
         for (int i = 0; i < classes.size(); i++) {
             System.out.println((i + 1) + " " + classes.get(i).getSimpleName());
+            Tshirt tshirt = new Tshirt();
+            Jeans jeans = new Jeans();
         }
-        Class C = getClass();
-        while (C != null) {
-            System.out.println(C.getName());
-            C = C.getSuperclass();
-
-
-        }
-        return null;
-    }
-}
-
-       /* int choice = sc.nextInt();
-        if (choice == 1) {
-            try {
-                Class<?> tclass = null;
-                try {
-                    tclass = Class.forName("src/Clothes/Tshirt.java");
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                Field[] declaredFields = tclass.getDeclaredFields();
-                for (Field field : declaredFields) {
-                    System.out.println(field);
-
-                }
-
-            }else{
-                try {
-                    Class<?> jclass = null;
-                    try {
-                        jclass = Class.forName("src/Clothes/Jeans.java");
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    Field[] declaredFields = jclass.getDeclaredFields();
-                    for (Field field : declaredFields) {
-                        System.out.println(field);
-
-                    }
-
-                    return null;
-                }
-            }
-        }
-
-    }
-}
+        /*new ReflectionFactory(List.of(Tshirt.class, Jeans.class));
+        Tshirt.class.getFields();
+        Jeans.class.getFields();
 */
+
+        Class C =getClass();
+            while (classes != null) {
+                System.out.println(C.getName());
+                C = C.getSuperclass();
+                Field[] fields = C.getFields();
+                for (Field field : fields) {
+                    Class<?> fld = field.getType();
+                    System.out.println("Class name:"+ field.getName());
+                    System.out.println("Class type:"+ fld.getName());
+                }
+
+            }
+            return null;
+
+        }
+
+    }
+
+
 //Поскольку в Java отсутствует множественное наследование,
 // то для получения всех предков следует рекурсивно вызвать метод getSuperclass() в цикле,
 // пока не будет достигнут Object, являющийся родителем всех классов. Object не имеет родителей,
 // поэтому вызов его метода getSuperclass() вернет null.
 
-/*  ::
-public static List<Class> getSuperClasses(Object o) {
-  List<Class> classList = new ArrayList<Class>();
-  Class class= o.getClass();
-  Class superclass = class.getSuperclass();
-  classList.add(superclass);
-  while (superclass != null) {
-    class = superclass;
-    superclass = class.getSuperclass();
-    classList.add(superclass);
-  }
-  return classList;
-}
- */
 
 
